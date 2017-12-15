5ce2057911dbd688d8072e9710af8df1707f0584
lmw8864 <lmw8864@gmail.com>
2017-12-15 11:32:53 +0900

    Create model Topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """사용자가 공부하고 있는 주제입니다."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """모델에 관한 정보를 문자열 형태로 반환합니다."""
+        return self.text
